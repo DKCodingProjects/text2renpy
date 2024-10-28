e5dcacdd6de8c7bc71d7ee970e7c21ff7450ba92
dkillinger <killinger.joseph@gmail.com>
2024-10-28 17:10:54 -0600

    Started translator for screenplay to renpy

---
+++ src/translate/screenplay_to_renpy.py
@@ -0,0 +1,8 @@
+from .translator  import Translator
+
+class Screenplay_to_Renpy(Translator):
+    def __init__(self):
+        super().__init__()
+    
+    def translate(self, text, attribs):
+        pass
