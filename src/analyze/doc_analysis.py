500bc1f3772f725e7a295d02078b5e14a9a6a229
dkillinger <killinger.joseph@gmail.com>
2024-10-31 13:38:52 -0600

    worked on doc_metadata and doc_analysis

--- src/analyze/doc_analysis.py
+++ src/analyze/doc_analysis.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractmethod
+from src.collect.doc_metadata import *
 
 class Document_Analysis(ABC):
     def __init__(self, read_file: str):
         self.file_name: str = read_file
     
     @abstractmethod
-    def analyze(self):
+    def analyze_except(self, err: Exception):
+        raise Exception('An error occured in {0} while analyzing file \'{1}\' ({2})'.format(self.__class__.__name__, self.file_name, f"{type(err).__name__}: {err}"))
+
+    @abstractmethod
+    def analyze(self) -> Document_Metadata:
         pass
--- src/collect/doc_metadata.py
+++ src/collect/doc_metadata.py
@@ -1,14 +1,34 @@
 from enum import Enum
+from pathlib import Path
 
 class Document_Types(Enum):
-    NONE = 0
-    RAW = 1
-    MARKDOWN = 2
-    FDX = 3
-    DOCX = 4
+    RAW = 0
+    MARKDOWN = 1
+    FDX = 2
+    DOCX = 3
 
 class Document_Metadata():
-    def __init__(self):
+    def find_type(file_name: str) -> Document_Types:
+        raw_files = {'.txt', '.rpy'}
+        md_files = {'.md', '.fountain'}
+        fdx_files = {'.fdx'}
+        doc_files = {'.docx'}
+
+        extension = Path(file_name).suffix
+        if extension in raw_files:
+            return Document_Types.RAW
+        elif extension in md_files:
+            return Document_Types.MARKDOWN
+        elif extension in fdx_files:
+            return Document_Types.FDX
+        elif extension in doc_files:
+            return Document_Types.DOCX
+        else:
+            raise TypeError('document \"{0}\" is not a supported document type for Text2RenPy'.format(file_name))
+
+    def __init__(self, doc_name: str):
+        self.file_name: str = doc_name
+        self.type = Document_Metadata.find_type(doc_name)
         self.font_size: float = None
         self.doc_length: int = None
     
