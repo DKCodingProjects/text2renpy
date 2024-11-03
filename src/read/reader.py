917adbc0f52b7ac8675e0471d286a4568c019605
dkillinger <killinger.joseph@gmail.com>
2024-11-02 19:47:24 -0600

    added Reader back in

--- src/read/reader.py
+++ src/read/reader.py
@@ -1,3 +1,24 @@
 from abc import ABC, abstractmethod
 from src.collect.text_chunk import Text_Chunk
 
+class Reader(ABC):
+
+    def __init__(self, read_file: str):
+        self.file_name: str = read_file
+        self.open_file = None
+        self.is_eof: bool = False
+    
+    @abstractmethod
+    def open_except(self, err: Exception):
+        raise Exception('An error occured in {0} while opening file \'{1}\' ({2})'.format(self.__class__.__name__, self.file_name, f"{type(err).__name__}: {err}"))
+
+    @abstractmethod
+    def open(self):
+        pass
+
+    def readpart_except(self, err: Exception):
+        raise Exception('Something went wrong with {0} in method \'readpart\' ({1})'.format(self.__class__.__name__, f"{type(err).__name__}: {err}"))
+
+    @abstractmethod
+    def readpart(self) -> tuple[list[Text_Chunk], dict]:
+        pass
