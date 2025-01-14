985cd11fd8825908ba57f65b98b24702b3a16715
dkillinger <killinger.joseph@gmail.com>
2025-01-14 15:06:52 -0700

    start home.py

---
+++ src/gui/window/home.py
@@ -0,0 +1,7 @@
+from PySide6.QtCore import QSize
+from PySide6.QtGui import QAction, QIcon
+from PySide6.QtWidgets import QMainWindow, QToolBar, QPushButton, QStatusBar
+
+class HomeWindow():
+    def __init__(self):
+        pass
