ead339108dc3a401f798ac8605478637ca4ffec5
dkillinger <killinger.joseph@gmail.com>
2025-03-24 17:36:39 -0600

    csv reader

--- src/read/csv_reader.py
+++ src/read/csv_reader.py
@@ -1,7 +1,7 @@
-from abc import ABC, abstractmethod
-from src.general.text_chunk import Text_Chunk
+import csv
+from read.reader import Reader
 
-class Reader(ABC):
+class Cav_Reader(Reader):
     def __init__(self, read_file: str):
         self.file_name: str = read_file
         self.open_file: any = None
@@ -11,13 +11,14 @@ class Reader(ABC):
     def _open_except(self, err: Exception):
         raise Exception('An error occured in {0} while opening file \'{1}\' ({2})'.format(self.__class__.__name__, self.file_name, f"{type(err).__name__}: {err}"))
 
-    @abstractmethod
     def open(self):
         pass
 
     def _readpart_except(self, err: Exception):
         raise Exception('Something went wrong with {0} in method \'readpart\' ({1})'.format(self.__class__.__name__, f"{type(err).__name__}: {err}"))
 
-    @abstractmethod
-    def readpart(self):
+    def readpart(self) -> list[str]:
+        pass
+
+    def readrow(self):
         pass
